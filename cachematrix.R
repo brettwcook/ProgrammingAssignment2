0eb36d09d1ac9168512677bcc5888e24c21b8da5
brettwcook <datascientistbwc>
2015-09-25 10:55:15 -0500

    Week 2 ProgAssignment
    Week 2 assignment code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code has two objectives:
+##  1.) Create a matrix which can cache data,
+##        then produce the inverse the cached data. 
+##  2.) Compute the inverse value of the cached data, if the inverse value
+##        does not already exist.
+##
+##The cache matrix creation function creates a matrix
+##  which is capable of storing a value and inverting
+##  that value
+##
+## Objective #1
+makeCachedMatrix <- function(x = matrix()){   
+	cd <- NULL                                ##Cached data
+	set <- function(s){                       
+		x <<- s                               
+		cd <<- NULL                           
+	}                                         
+	get <- function() x                       
+	setinv <- function(solve) cd <<- solve    
+	getinv <- function() cd                   
+	list(set = set, get = get,                
+		setinv = setinv,                      
+		getinv = getinv)                      
+}                                             
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+##The cacheSolve function collect the cached value
+##  adn performs a computation 
+##
+## Objective #2
+cacheSolve <- function(x = matrix(), ...) {   
+	cd <- x$getinv()                          
+	if(!is.null(cd)) {                        
+		message("Getting cached data")        
+		return(cd)                            
+	}                                         
+	data <- x$get()                           
+	cd <- solve(data, ...)                    
+	x$setinv(cd)                              
+	cd                                        
+}                                             
